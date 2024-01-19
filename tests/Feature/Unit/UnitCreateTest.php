b8df27f6caf338b42a05f557ed981c921a0b85b4
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-01-19 18:09:27 -0300

    DSIS-1714: Refactor unit creation test and add test for unauthenticated user

--- tests/Feature/Unit/UnitCreateTest.php
+++ tests/Feature/Unit/UnitCreateTest.php
@@ -5,7 +5,7 @@ use App\Models\User;
 
 it('should be able to create a unit', function () {
     $user = User::factory()->create();
-    
+
     $response = $this->actingAs($user)->post(route('units.store'), [
         'name'        => 'Unit Test',
         'description' => 'Description of Unit Test',
@@ -17,7 +17,7 @@ it('should be able to create a unit', function () {
     expect($response)
         ->assertRedirect(route('home'))
         ->assertSessionHas('success', 'Unidade criada com sucesso.');
-    
+
     $unit = Unit::where('name', 'Unit Test')->first();
     expect($unit->name)->toBe('Unit Test');
     expect($unit->description)->toBe('Description of Unit Test');
@@ -26,4 +26,16 @@ it('should be able to create a unit', function () {
     expect((float)$unit->latitude)->toBe(-23.5505199);
     expect((float)$unit->longitude)->toBe(-46.6333094);
 });
+
+it('should redirect unauthenticated user to login page', function () {
+    $response = $this->post(route('units.store'), [
+        'name'        => 'Unit Test',
+        'description' => 'Description of Unit Test',
+        'email'       => 'unit@test.com',
+        'phone'       => '123456789',
+        'latitude'    => -23.5505199,
+        'longitude'   => -46.6333094,
+    ]);
+
+    expect($response)->assertRedirect(route('login'));
 });
