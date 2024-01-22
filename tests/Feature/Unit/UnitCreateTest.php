f49e1fcc6ceb14444c9c2d8345c4d9f84ff639e3
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-01-21 23:14:07 -0300

    DSIS-1714: Add unit creation tests for validation errors

--- tests/Feature/Unit/UnitCreateTest.php
+++ tests/Feature/Unit/UnitCreateTest.php
@@ -3,6 +3,8 @@
 use App\Models\Unit;
 use App\Models\User;
 
+use function Pest\Laravel\post;
+
 it('should be able to create a unit', function () {
     $user = User::factory()->create();
 
@@ -38,4 +40,103 @@ it('should redirect unauthenticated user to login page', function () {
     ]);
 
     expect($response)->assertRedirect(route('login'));
+});
+
+it('should return error when unit name is not provided', function () {
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->post(route('units.store'), [
+        'description' => 'Description of Unit Test',
+        'email'       => 'unit@test.com',
+        'phone'       => '123456789',
+        'latitude'    => -23.5505199,
+        'longitude'   => -46.6333094,
+    ]);
+
+    expect($response)->assertSessionHasErrors(['name' => 'The name field is required.']);
+});
+
+it('should return error when unit description is not provided', function () {
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->post(route('units.store'), [
+        'name'     => 'Unit Test',
+        'email'       => 'unit@test.com',
+        'phone'       => '123456789',
+        'latitude'    => -23.5505199,
+        'longitude'   => -46.6333094,
+    ]);
+
+    expect($response)->assertSessionHasErrors(['description' => 'The description field is required.']);
+});
+
+it('should return error when unit email is not provided', function () {
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->post(route('units.store'), [
+        'name'        => 'Unit Test',
+        'description' => 'Description of Unit Test',
+        'phone'       => '123456789',
+        'latitude'    => -23.5505199,
+        'longitude'   => -46.6333094,
+    ]);
+
+    expect($response)->assertSessionHasErrors(['email' => 'The email field is required.']);
+});
+
+it('should return error when unit phone is not provided', function () {
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->post(route('units.store'), [
+        'name'        => 'Unit Test',
+        'description' => 'Description of Unit Test',
+        'email'       => 'unit@test.com',
+        'latitude'    => -23.5505199,
+        'longitude'   => -46.6333094,
+    ]);
+
+    expect($response)->assertSessionHasErrors(['phone' => 'The phone field is required.']);
+});
+
+it('should return error when unit latitude is not provided', function () {
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->post(route('units.store'), [
+        'name'        => 'Unit Test',
+        'description' => 'Description of Unit Test',
+        'email'       => 'unit@test.com',
+        'phone'       => '123456789',
+        'longitude'   => -46.6333094,
+    ]);
+
+    expect($response)->assertSessionHasErrors(['latitude' => 'The latitude field is required.']);
+});
+
+it('should return error when unit longitude is not provided', function () {
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->post(route('units.store'), [
+        'name'        => 'Unit Test',
+        'description' => 'Description of Unit Test',
+        'email'       => 'unit@test.com',
+        'phone'       => '123456789',
+        'latitude'    => -23.5505199,
+    ]);
+
+    expect($response)->assertSessionHasErrors(['longitude' => 'The longitude field is required.']);
+});
+
+it('should return error when email format is invalid', function () {
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->post(route('units.store'), [
+        'name'        => 'Unit Test',
+        'description' => 'Description of Unit Test',
+        'email'       => 'invalid-email-format',
+        'phone'       => '123456789',
+        'latitude'    => -23.5505199,
+        'longitude'   => -46.6333094,
+    ]);
+
+    expect($response)->assertSessionHasErrors(['email' => 'The email field must be a valid email address.']);
 });
