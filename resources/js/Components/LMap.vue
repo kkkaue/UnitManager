5f4e2d80ef5806922ac3447bd1331df5bdb272ba
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-02-15 01:57:12 -0300

    DSIS-1781: Adicionada lógica para exibir um marcador com base em um valor recebido, ajuste de zoom e controle de interação baseado na propriedade 'disabled'

--- resources/js/Components/LMap.vue
+++ resources/js/Components/LMap.vue
@@ -3,6 +3,18 @@
 import Leaflet from 'leaflet';
 import { ref, onMounted, defineEmits } from 'vue';
 
+// Definindo as propriedades que serão recebidas do componente pai
+const props = defineProps({
+  modelValue: {
+    type: Object,
+    required: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+
 // Criando referências reativas para o mapa e o marcador
 const map = ref(null);
 const mapMarker = ref(null);
@@ -12,28 +24,52 @@ const emitEvent = defineEmits(['update:modelValue']);
 
 // Função que será executada após o componente ser montado
 onMounted(() => {
-  // Inicializando o mapa e definindo a visão inicial
-  map.value = Leaflet.map('map').setView([0.0396198, -51.0455364], 12);
-  
-  // Adicionando a camada de tiles do OpenStreetMap ao mapa
-  Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map.value);
-
-  // Adicionando um ouvinte de evento de clique ao mapa
-  map.value.on('click', function(event) {
-    // Se já existe um marcador, removê-lo antes de adicionar um novo
-    if (mapMarker.value) {
-      map.value.removeLayer(mapMarker.value);
-      mapMarker.value = Leaflet.marker([event.latlng.lat, event.latlng.lng]).addTo(map.value);
-    } else {
-      // Adicionando um novo marcador ao mapa nas coordenadas do clique
-      mapMarker.value = Leaflet.marker([event.latlng.lat, event.latlng.lng]).addTo(map.value);
-    }
+  if (!props.modelValue || !props.modelValue.lat || !props.modelValue.lng) {
+    map.value = Leaflet.map('map').setView([0.0396198, -51.0455364], 12);
     
-    // Emitindo um evento para atualizar o valor do modelo no componente pai com as coordenadas do marcador
-    emitEvent('update:modelValue', { lat: event.latlng.lat, lng: event.latlng.lng });
-  });
+    Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map.value);
+
+    if (!props.disabled) {
+      // Adicionando um ouvinte de evento de clique ao mapa
+      map.value.on('click', function(event) {
+        if (mapMarker.value) {
+          map.value.removeLayer(mapMarker.value);
+          mapMarker.value = Leaflet.marker([event.latlng.lat, event.latlng.lng]).addTo(map.value);
+        } else {
+          mapMarker.value = Leaflet.marker([event.latlng.lat, event.latlng.lng]).addTo(map.value);
+        }
+        
+        // Emitindo um evento para atualizar o valor do modelo no componente pai com as coordenadas do marcador
+        emitEvent('update:modelValue', { lat: event.latlng.lat, lng: event.latlng.lng });
+      });
+    }
+  } else {
+    map.value = Leaflet.map('map').setView([props.modelValue.lat, props.modelValue.lng], 20);
+
+    Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map.value);
+
+    if (!props.disabled) {
+      // Adicionando um ouvinte de evento de clique ao mapa
+      map.value.on('click', function(event) {
+        if (mapMarker.value) {
+          map.value.removeLayer(mapMarker.value);
+          mapMarker.value = Leaflet.marker([event.latlng.lat, event.latlng.lng]).addTo(map.value);
+        } else {
+          mapMarker.value = Leaflet.marker([event.latlng.lat, event.latlng.lng]).addTo(map.value);
+        }
+        
+        // Emitindo um evento para atualizar o valor do modelo no componente pai com as coordenadas do marcador
+        emitEvent('update:modelValue', { lat: event.latlng.lat, lng: event.latlng.lng });
+      });
+    }
+
+    // Adicionando um marcador ao mapa nas coordenadas do modelo
+    mapMarker.value = Leaflet.marker([props.modelValue.lat, props.modelValue.lng]).addTo(map.value);
+  }
 });
 </script>
 
