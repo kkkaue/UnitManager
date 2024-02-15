dd5fd5acb00d80379c7bf90ec95e9ac424aa295e
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-02-15 01:58:31 -0300

    DSIS-1781: Refatoração do método de atualização de unidades no UnitController

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Unit;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use Inertia\Inertia;
 
 class UnitController extends Controller
@@ -105,16 +106,60 @@ class UnitController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        $request->validate([
+        $rules = [
             'name'        => ['required', 'string', 'max:255'],
             'description' => ['required', 'string'],
-            'email'       => ['required', 'email', 'max:255', 'unique:units'],
+            'email'       => ['required', 'email', 'max:255', Rule::unique('units')->ignore($id)],
             'phone'       => ['required', 'string', 'max:255'],
+            'location'    => ['required', 'array'], // 'latitude' e 'longitude
             'latitude'    => ['required', 'numeric'],
             'longitude'   => ['required', 'numeric'],
-        ]);
+            'parent_id'   => 'nullable|exists:units,id',
+        ];
+        if ($id !== '1') {
+            $rules['parent_id'] = 'required|exists:units,id';
+        }
+
+        $messages = [
+            'name.required'        => 'O campo nome é obrigatório.',
+            'name.max'             => 'O campo nome deve ter no máximo 255 caracteres.',
+            'description.required' => 'O campo descrição é obrigatório.',
+            'email.required'       => 'O campo e-mail é obrigatório.',
+            'email.email'          => 'O e-mail informado é inválido.',
+            'email.max'            => 'O campo e-mail deve ter no máximo 255 caracteres.',
+            'email.unique'         => 'Este e-mail já está em uso.',
+            'phone.required'       => 'O campo telefone é obrigatório.',
+            'phone.max'            => 'O campo telefone deve ter no máximo 255 caracteres.',
+            'latitude.required'    => 'Você deve informar a a localização da unidade.',
+            'longitude.required'   => 'Você deve informar a a localização da unidade.',
+            'parent_id.required'   => 'Você deve informar a unidade pai.',
+            'parent_id.exists'     => 'A unidade pai informada não existe.',
+            'parent_id.child'      => 'A unidade não pode ser atualizada para ser filha de uma de suas unidades filhas.',
+        ];
+
+        $request->validate($rules, $messages);
+
+        $data = [
+            'name'        => $request->name,
+            'description' => $request->description,
+            'email'       => $request->email,
+            'phone'       => $request->phone,
+            'latitude'    => $request->latitude,
+            'longitude'   => $request->longitude,
+        ];
+        if ($request->has('parent_id')) {
+            $data['parent_id'] = $request->parent_id;
+        }
+
         $unit = Unit::findOrFail($id);
-        $unit->update($request->all());
+
+        // Verifica se a unidade não está sendo atualizada para ser filha de uma de suas unidades filhas
+        $hasChildWithSameId = $unit->children()->where('id', $data['parent_id'])->exists();
+        if ($hasChildWithSameId) {
+            return redirect()->back()->withErrors(['parent_id' => $messages['parent_id.child']]);
+        }
+        
+        $unit->update($data);
 
         return redirect()->back()->with('success', 'Unidade atualizada com sucesso.');
     }
