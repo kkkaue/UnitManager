bd5c639c1b3f51067120ad928d628788d0d99f30
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-02-15 03:48:08 -0300

    DSIS-1782: Atualizações no formulário de edição de unidade

--- resources/js/Components/unitModal/EditUnitModal.vue
+++ resources/js/Components/unitModal/EditUnitModal.vue
@@ -32,8 +32,21 @@ const props = defineProps({
     },
 });
 
+// Definindo a referência para o estado do modal de confirmação
+const isConfirmationModalOpen = ref(false);
+
 // Definindo a referência para o estado do formulário
 const markerPosition = ref({ lat: 0, lng: 0 });
+const initialUnitForm = {
+    name: null,
+    description: null,
+    email: null,
+    phone: null,
+    location: null,
+    latitude: null,
+    longitude: null,
+    parent_id: null,
+};
 
 // Inicializando o formulário com valores nulos
 const unitForm = useForm({
@@ -49,6 +62,8 @@ const unitForm = useForm({
 
 // Função para fechar o modal
 const closeUnitModal = () => {
+    isConfirmationModalOpen.value = false;
+    resetUnitForm();
     props.onClosed();
 };
 
@@ -64,6 +79,17 @@ const submitUnitForm = () => {
     });
 };
 
+const resetUnitForm = () => {
+    unitForm.name = initialUnitForm.name;
+    unitForm.description = initialUnitForm.description;
+    unitForm.email = initialUnitForm.email;
+    unitForm.phone = initialUnitForm.phone;
+    unitForm.location = initialUnitForm.location;
+    unitForm.latitude = initialUnitForm.latitude;
+    unitForm.longitude = initialUnitForm.longitude;
+    unitForm.parent_id = initialUnitForm.parent_id;
+};
+
 watch(() => props.unit, (newValue) => {
     unitForm.name = newValue.name;
     unitForm.description = newValue.description;
@@ -73,6 +99,16 @@ watch(() => props.unit, (newValue) => {
     unitForm.latitude = newValue.latitude;
     unitForm.longitude = newValue.longitude;
     unitForm.parent_id = newValue.parent_id ? newValue.parent_id.toString() : null;
+
+    // Armazenando os valores iniciais do formulário
+    initialUnitForm.name = newValue.name;
+    initialUnitForm.description = newValue.description;
+    initialUnitForm.email = newValue.email;
+    initialUnitForm.phone = newValue.phone;
+    initialUnitForm.location = newValue.location;
+    initialUnitForm.latitude = newValue.latitude;
+    initialUnitForm.longitude = newValue.longitude;
+    initialUnitForm.parent_id = newValue.parent_id ? newValue.parent_id.toString() : null;
 });
 
 // Observando a posição do marcador e atualizando os valores de latitude e longitude do formulário
@@ -195,7 +231,37 @@ onMounted(() => {
                 <Button variant="destructive" @click="closeUnitModal">
                     Fechar
                 </Button>
-                <Button @click="submitUnitForm">
+                <Button @click="isConfirmationModalOpen = true">
+                    Editar
+                </Button>
+            </CardFooter>
+        </Card>
+    </div>
+    <!-- Modal de Confirmação de edição unidade -->
+    <div v-if="isConfirmationModalOpen"
+    class="fixed inset-0 z-10 overflow-y-auto flex items-center justify-center bg-black bg-opacity-50" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <Card class="w-1/3">
+            <!-- Cabeçalho do modal -->
+            <CardHeader>
+                <CardTitle>
+                    Editar Unidade
+                </CardTitle>
+                <CardDescription>
+                    Tem certeza que deseja editar a unidade {{ unit.name }}?
+                </CardDescription>
+            </CardHeader>
+            <!-- Conteúdo do modal -->
+            <CardContent>
+                <p class="text-sm text-gray-500">
+                    Ao editar a unidade, todos os dados relacionados a ela serão atualizados.
+                </p>
+            </CardContent>
+            <!-- Rodapé do modal -->
+            <CardFooter class="flex justify-between px-6 pb-6">
+                <Button type="button" variant="destructive" @click="closeUnitModal">
+                    Cancelar
+                </Button>
+                <Button type="button" @click="submitUnitForm">
                     Salvar
                 </Button>
             </CardFooter>
